e5e22380800551dfa805488a9484a12e5c423eb8
gruhitsc4k <monica22patel+1@gmail.com>
2026-02-28 12:59:20 -0500

    added ajeesh's code

--- ajeesh/conditional_statements/Main.java
+++ ajeesh/conditional_statements/Main.java
@@ -7,13 +7,19 @@ public class Main{
         System.out.print("Enter a number:- ");
         int num = in.nextInt();
 
-        if(num > 0){
-            System.out.println(num + " is a positive number.");
-        }else if(num < 0){
-            System.out.println(num + " is a negative number.");
-        }
-        else{
-            System.out.println(num + " is a neutral number.");
+        /*
+            Write a program to tae a person's age and inform them about their age grop.
+            1. < 12 - kid
+            2. 12 - 18 - Teen
+            3. 19-30 - Youth
+            4. 31-60 - Adult
+            5. > 61 - Senior 
+        */
+
+        if(num % 2 == 0 && num > 0){
+            System.out.println("You have the right number.");
+        }else{
+            System.out.println("The number is invalid.");
         }
     }
 }
